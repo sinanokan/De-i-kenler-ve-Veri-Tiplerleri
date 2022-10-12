fa67476809a81b575f841d7910ed6afca222607d
sinan okan <okanyurtsever14@gmail.com>
2022-10-12 14:17:50 +0300

    ekleme silme

--- Operatorler/obj/Debug/net6.0/Operatorler.AssemblyInfo.cs
+++ Operatorler/obj/Debug/net6.0/Operatorler.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("csProjeler")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Operatorler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("csProjeler")]
-[assembly: System.Reflection.AssemblyTitleAttribute("csProjeler")]
+[assembly: System.Reflection.AssemblyProductAttribute("Operatorler")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Operatorler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
